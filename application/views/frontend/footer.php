2e2323ead366eb7a0811e8275d8000c2ffcf4517
Ubuntu <ubuntu@ip-172-30-0-168.ap-southeast-1.compute.internal>
2017-08-19 12:28:29 +0000

    Live changes

--- application/views/frontend/footer.php
+++ application/views/frontend/footer.php
@@ -719,3 +719,13 @@ function checkRegisterValidation(){
     
 });
   </script>
+<!--Start of Zendesk Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){
+z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+$.src='https://v2.zopim.com/?52ozvORp8qcpuZPXxA7YwgdqWauyIM3v';z.t=+new Date;$.
+type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+</script>
+<!--End of Zendesk Chat Script-->
